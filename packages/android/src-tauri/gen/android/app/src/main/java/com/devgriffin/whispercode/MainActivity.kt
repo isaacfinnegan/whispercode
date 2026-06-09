8b6fc5fc645dd645616d932509b7f5e4eb589043
isaacfinnegan <952846+isaacfinnegan@users.noreply.github.com>
2026-06-09 13:35:47 -1000

    feat(android): permit cleartext HTTP and self-signed SSL certs in debug builds

--- packages/android/src-tauri/gen/android/app/src/main/java/com/devgriffin/whispercode/MainActivity.kt
+++ packages/android/src-tauri/gen/android/app/src/main/java/com/devgriffin/whispercode/MainActivity.kt
@@ -2,10 +2,51 @@ package com.devgriffin.whispercode
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import android.webkit.*
+import android.graphics.Bitmap
+import android.net.http.SslError
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
   }
+
+  override fun onWebViewCreate(webView: WebView) {
+    super.onWebViewCreate(webView)
+    val originalClient = webView.webViewClient
+    if (originalClient != null) {
+      webView.webViewClient = DelegatingWebViewClient(originalClient)
+    }
+  }
+}
+
+class DelegatingWebViewClient(private val delegate: WebViewClient) : WebViewClient() {
+    override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? {
+        return delegate.shouldInterceptRequest(view, request)
+    }
+
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        return delegate.shouldOverrideUrlLoading(view, request)
+    }
+
+    override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+        delegate.onPageStarted(view, url, favicon)
+    }
+
+    override fun onPageFinished(view: WebView, url: String) {
+        delegate.onPageFinished(view, url)
+    }
+
+    override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
+        delegate.onReceivedError(view, request, error)
+    }
+
+    override fun onReceivedSslError(view: WebView, handler: SslErrorHandler, error: SslError) {
+        if (BuildConfig.DEBUG) {
+            handler.proceed()
+        } else {
+            delegate.onReceivedSslError(view, handler, error)
+        }
+    }
 }
