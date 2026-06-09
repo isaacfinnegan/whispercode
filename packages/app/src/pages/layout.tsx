3000004ae79e6937e0f78adfd6ed3638b4606810
Isaac Finnegan <952846+isaacfinnegan@users.noreply.github.com>
2026-06-09 09:02:40 -1000

    feat(layout): sync push state in onMount

--- packages/app/src/pages/layout.tsx
+++ packages/app/src/pages/layout.tsx
@@ -242,6 +242,10 @@ export default function Layout(props: ParentProps) {
     setNotificationOpen((value) => {
       void openPush(value)
     })
+    if (platform.pushState) {
+      // Sync push pairing & relay tokens
+      platform.pushState()
+    }
     onCleanup(() => {
       setNavigate(undefined as any)
       setNotificationOpen(undefined)
