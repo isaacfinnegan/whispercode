55b798429cf5b7b7d004618ed53e6fcd20a9c901
isaacfinnegan <952846+isaacfinnegan@users.noreply.github.com>
2026-06-09 20:24:47 -1000

    chore(android): add tauri http plugin dependencies and register it

--- packages/android/src-tauri/src/lib.rs
+++ packages/android/src-tauri/src/lib.rs
@@ -2,6 +2,7 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_haptics::init())
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::new().build())
