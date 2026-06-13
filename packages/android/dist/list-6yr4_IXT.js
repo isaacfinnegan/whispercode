99d9633d4079118c737a5aa20e1696a2a6422c8c
isaacfinnegan <952846+isaacfinnegan@users.noreply.github.com>
2026-06-13 06:29:00 -1000

    chore(android): rebuild assets after upstream merge

--- packages/android/dist/list-6yr4_IXT.js
+++
@@ -1,1 +0,0 @@
-import{r as e,H as i}from"./index-BRi2ZOi5.js";/* empty css                    */var n=i("<div data-component=settings-v2-list>");const o=r=>(()=>{var t=n();return e(t,()=>r.children),t})();export{o as SettingsListV2};
