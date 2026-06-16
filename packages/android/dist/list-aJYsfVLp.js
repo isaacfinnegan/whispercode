494453505f993449b443cb3ec76201b0015f9e6d
isaacfinnegan <952846+isaacfinnegan@users.noreply.github.com>
2026-06-16 04:28:00 -1000

    chore(android): rebuild assets

---
+++ packages/android/dist/list-aJYsfVLp.js
@@ -0,0 +1,1 @@
+import{r as e,H as i}from"./index-Cs5Q-Gw0.js";/* empty css                    */var n=i("<div data-component=settings-v2-list>");const o=r=>(()=>{var t=n();return e(t,()=>r.children),t})();export{o as SettingsListV2};
